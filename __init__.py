d6e923ea2b9f1f104be9deb7540ca4ab5a3d1701
gespinoza <gespinoza@utexas.edu>
2016-04-19 10:51:44 +0200

    Revert "Changes in descriptions"
    This reverts commit ae99ae231098be6ebedc4de0505952ab6debaf93.

--- __init__.py
+++ __init__.py
@@ -9,9 +9,8 @@ Module: wa
 
 Description:
 This module contains a compilation of scripts and functions used to download
-hydrologic, atmospheric, and remote sensing data from different sources.
-This data is used within a water accounting framework.
-(http://www.wateraccounting.org/)
+hydrologic data from different sourcers. The hydrologic data downloaded can be
+used for water accounting (http://www.wateraccounting.org/)
 """
 
 from wa import Level1
