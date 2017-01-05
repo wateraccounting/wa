2c1c3507d374d028ffce03249d77625eb32f7b88
TimHessels <timhessels@hotmail.com>
2017-01-05 08:36:55 +0100

    New path added

--- WA_Paths.py
+++ WA_Paths.py
@@ -10,9 +10,10 @@ Module: -
 def Paths(Type=None):
     
     User_Path = {
-    'GDAL'                 :   r'C:\Program Files (x86)\GDAL',           #folder
-    '7z.exe'               :   r'C:\Program Files (x86)\GDAL\7z.exe',    #complete path to executable
-    'curl.exe'             :   r'C:\Program Files (x86)\GDAL\curl.exe'   #complete path to executable
+    'Home'                 :   r'',   #directory to the wa folder
+    'GDAL'                 :   r'',   #folder
+    '7z.exe'               :   r'',   #complete path to executable
+    'curl.exe'             :   r''    #complete path to executable
     }
     
     Selected_Path = User_Path[Type]    
