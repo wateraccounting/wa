d374074a509ee287ef855d0adc292663cba75fb2
TimHessels <timhessels@hotmail.com>
2018-05-22 21:11:31 +0200

    Add ASCAT data

--- WebAccounts.py
+++ WebAccounts.py
@@ -5,14 +5,16 @@ Created on Thu Mar 17 09:27:36 2016
 @author: tih
 """
 
+
 def Accounts(Type=None):
-    
+
     User_Pass = {
-     'NASA'                  :   ['',''],   
-     'GLEAM'                 :   ['',''],  
-     'FTP_WA'                :	 ['',''],
-     'MSWEP'                :	 ['',''] }
-    
-    Selected_Path = User_Pass[Type]    
-    
+     'NASA': ['', ''],
+     'GLEAM': ['', ''],
+     'FTP_WA': ['', ''],
+     'MSWEP': ['', ''],
+     'VITO': ['', '']}
+
+    Selected_Path = User_Pass[Type]
+
     return(Selected_Path)
